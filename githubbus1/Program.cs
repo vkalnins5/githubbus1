f5297111240631973e42014b19010da745c1ad63
vkalnins5 <vkalnins5@DX02043376>
2022-01-12 08:39:43 +0200

    Add project files.

---
+++ githubbus1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace githubbus1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yo no v4 wassup\n");
+        }
+    }
+}
